f10ece1d436b825ac5faa345e8ac107f4881acf6
“milliybro” <shohrux-rustamov@mail.ru>
2023-06-21 12:00:34 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+window.addEventListener("scroll", function () {
+   shrink();
+ });
+ 
+ let navbar = document.getElementById("navbar");
+ 
+ function shrink() {
+   if (scrollY > 100) {
+     navbar.classList.add("navbar-shrink");
+   } else {
+       navbar.classList.remove("navbar-shrink");
+    }
+ }
+
+ window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.classList.add('backtop-show')
+  } else {
+    backtop.classList.remove('backtop-show')
+  }
+}
+
+function openNavbar() {
+  document.getElementById("navbar-responsive").style.left = "0";
+  // document.getElementById("navbar-responsive").style.top = "0";
+}
+function closeNavbar() {
+  document.getElementById("navbar-responsive").style.left = "-100%";
+  // document.getElementById("navbar-responsive").style.top = "-100%";
+}
+
+document.getElementById("navbar-open").addEventListener("click", openNavbar);
+document.getElementById("navbar-close").addEventListener("click", closeNavbar);
